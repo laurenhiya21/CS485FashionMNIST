a649674ec3673cc3e5637faeeddeb705d8ce71e5
Hossam Basiony <basionyhossam@gmail.com>
2018-12-09 15:02:01 -0800

    Read from matrices and labels

--- fashionStart.m
+++ fashionStart.m
@@ -1,22 +1,24 @@
 %the inputs of the neural network
 %----------------------------------
 
-%set this to part 1 to run the part 1 code
-assignmentPart = 2;
-
 %the matricies for the inputs + labels
-global outputLabels;
-global inputMatrices;
+global csvLabels;
+global csvInput;
 global outputMatricies;
+global outputSize;
+%the number of inputs
+global numberOfInputs;
+global trainData;
 
-%get the input/labels depending on which part of the assignment
-if assignmentPart == 2
+
     %get the inputs and labels from MNIST
-    outputLabels = loadMNISTLabels('t10k-labels.idx1-ubyte');
-    inputMatrices = loadMNISTImages('t10k-images.idx3-ubyte');
+    trainData = csvread('train.csv',1,1);
+   
+    csvLabels = trainData(:,1);
+   
+    csvInput = csvread('train.csv',1,2);
     
-    CSVLabels = csvread('train.csv',1,0);
-    
+    csvInput = csvInput';
     
     %the matrix represenation of each label
     t0 = [1;0;0;0;0;0;0;0;0;0];
@@ -33,36 +35,16 @@ if assignmentPart == 2
     %put them all in a single matrix
     outputMatricies = [t0 t1 t2 t3 t4 t5 t6 t7 t8 t9];
     
-elseif assignmentPart == 1 %If we are on part 1 of the assignment
-        
-    %Get the input from somewhere for the network, for now hardcoded vectors
-    p0 = [0;1;1;1;1;0;1;0;0;0;0;1;1;0;0;0;0;1;1;0;0;0;0;1;0;1;1;1;1;0];
-    p1 = [0;0;0;0;0;0;1;0;0;0;0;0;1;1;1;1;1;1;0;0;0;0;0;0;0;0;0;0;0;0];
-    p2 = [1;0;0;0;0;0;1;0;0;1;1;1;1;0;0;1;0;1;0;1;1;0;0;1;0;0;0;0;0;1];
-
-    %for checking noisy
-    p3 = applyNoise(p0,4);
-    p4 = applyNoise(p1,4);
-    p5 = applyNoise(p2,4);
-
-    p6 = applyNoise(p0,8);
-    p7 = applyNoise(p1,8);
-    p8 = applyNoise(p2,8);
-
-    %get the desired output from somewhere, for now hardcoded
-    outputLabels = [0;1;2;0;1;2;0;1;2];
-
-    %put the input into a matrix
-    inputMatrices = [p0 p1 p2];
-
-    %the matrix represenation of each label
-    t0 = [1;0;0];
-    t1 = [0;1;0];
-    t2 = [0;0;1];
-
-    %put them all in a single matrix
-    outputMatricies = [t0 t1 t2];
-end
+    %Transposing outputMatricies
+    outputMatricies = outputMatricies';
+    
+    %Uncomment this line to verify that csvLabels is working correctly
+    %disp(csvLabels);
+ 
+    %Uncomment this line to verify that csvInput is working correctly
+    %disp(csvInput) 
+     
+    
 
 %parameters neural network
 %----------------------------------
@@ -73,16 +55,13 @@ hiddenSize = 8;
 %the size of the neural network (autogenerate later, hardcode for now)
 %------------------------
 %The size of the output layer (0,1 and 2 would be 3 possible outcomes)
-global outputSize;
 
 outputSize = size(outputMatricies,2);
 
-%the number of inputs
-global numberOfInputs;
-numberOfInputs = size(inputMatrices,2);
+numberOfInputs = size(csvInput,2);
 
 %The size of the input vector
-inputSize = size(inputMatrices,1);
+inputSize = size(csvInput,1);
 
 %intialize the neural network
 %----------------------------------
@@ -117,53 +96,7 @@ xlabel("Iteration");
 %run the network
 %----------------------------------
 
-if assignmentPart == 1
-    %train the network on the first 3 (0, 1, 2)
-    runNetwork(200, 2, true, false, false);
-    
-    %test the clean data
-    cost = runNetwork(1,2,false,false, false);
-    disp("clean cost: " + cost);
-    
-    %test the slightly noisy data
-    inputMatrices = [p3 p4 p5];
-    cost = runNetwork(1,2,false,false, false);
-    disp("kinda noisy cost: " + cost);
-    
-    %test the really noisy data
-    inputMatrices = [p6 p7 p8];
-    cost = runNetwork(1,2,false,false, false);
-    disp("very noisy cost: " + cost);
-    
-    %train the network on all 9 (0, 1, 2)
-    disp("training the network on some noisy data...");
-    inputMatrices = [p0 p1 p2 p3 p4 p5 p6 p7 p8];
-    numberOfInputs = size(inputMatrices,2);
-    runNetwork(200, 2, true, true, false);
-    
-    %test the slightly noisy data
-    inputMatrices = [p0 p1 p2];
-    numberOfInputs = size(inputMatrices,2);
-    cost = runNetwork(1,2,false,false, false);
-    disp("clean cost: " + cost);
-    
-    %test the slightly noisy data
-    inputMatrices = [p3 p4 p5];
-    cost = runNetwork(1,2,false,false, false);
-    disp("kinda noisy cost: " + cost);
-    
-    %test the really noisy data
-    inputMatrices = [p6 p7 p8];
-    cost = runNetwork(1,2,false,false, false);
-    disp("very noisy cost: " + cost);
-    
-    %train the network on all 9 (0, 1, 2)
-    inputMatrices = [p0 p1 p2 p3 p4 p5 p6 p7 p8];
-    numberOfInputs = size(inputMatrices,2);
-    correct = runNetwork(1, 2, false, false, true);
-    disp("After training: " + correct);
 
-elseif assignmentPart == 2
     %run the neural network with 200 iterations
     runNetwork(200, 2, true, true, false);
     
@@ -174,7 +107,7 @@ elseif assignmentPart == 2
 
     %show the output
     disp("correct: " + correct );
-end
+
 
 
 %the network function
@@ -188,8 +121,8 @@ end
 function c = runNetwork(t, k, l, g, r)
 
     %get the input matricies
-    global inputMatrices;
-    global outputLabels;
+    global csvInput;
+    global csvLabels;
     global outputMatricies;
     
     %get the weights
@@ -218,12 +151,12 @@ function c = runNetwork(t, k, l, g, r)
         numCorrect = 0;
  
         %for each of the inputs in the batch
-        for i = 1:(numberOfInputs)
+        for i = 1:(100)
             %get the corresponding input
-            inputVec = inputMatrices(:,i);
+            inputVec = csvInput(:,i);
           
             %get the label of the output
-            label = outputLabels(i,:) + 1; 
+            label = csvLabels(i,:) + 1; 
    
             %get the desired output matrix from the label
             desiredOutput = outputMatricies(:,label);
@@ -323,23 +256,7 @@ function n = netOutput(w,p,b)
 n = (w * p) + b;
 end
 
-% randomly changes 2 of the "pixels" in an image
-function n = applyNoise(p,num)
-xmin= 1;
-xmax= numel(p);
-values =  int8(xmin+rand(1,num)*(xmax-xmin));
 
-for i = 1:numel(values)
-    if(p(values(i)) == 0)
-       p(values(i)) = 1;
-    else
-       p(values(i)) = 0;
-    end
-end
-
-n = p;
-
-end
 
 %code from http://ufldl.stanford.edu/wiki/index.php/Using_the_MNIST_Dataset
 function images = loadMNISTImages(filename)
